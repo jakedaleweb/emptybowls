c8bc8c6ae4be1248ee541bd52576ec5797c23688
twowangosaurus <jake.dale@xtra.co.nz>
2015-04-29 17:09:10 +1200

    changes to map functionality

--- js/maps.js
+++ js/maps.js
@@ -1,62 +1,114 @@
-function initialize() {
+//set up variables to be used between diff functions
+var map;
+var supportersList;
+var markerCollection = [];
 
-  //set locations as empty array
-  var locations = [];
+//set up icons object
+var ICONS = {
+'UNSELECTED': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
+'SELECTED':   'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+};
 
-  //get locations from db
-  $.ajax({
-    dataType: "json",
-      url: "js/maps.json",
-    success: function(data){
-      $(data).each(function(i){
-        var location = [ data[i].name, data[i].link, new google.maps.LatLng(data[i].lat, data[i].lng) ];
-        locations.push(location);
-        //set up the google objects needed
-        var bounds = new google.maps.LatLngBounds();
-        var infowindow = new google.maps.InfoWindow();  
-        //set up map options
-        var mapOptions = {
-          zoom: 20
-        }
-        //add location to list
-        $('#supporters').append('<li><a href="'+locations[i][1]+'">'+locations[i][0]+'</a></li>')
-        //create map
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
-        //loop through array to place markers
-        for (var i = 0; i < locations.length; i++) {  
-          var marker = new google.maps.Marker({
-            position: locations[i][2],
-            map: map
-          });
+//run everything
+function initialise() {
+  //set up map
+  var mapOptions = {
+    center: new google.maps.LatLng(-41.128314,174.944385),
+    zoom: 9
+  };
+  map = new google.maps.Map(document.querySelector("#map-canvas"), mapOptions);
+  //capture supporters ul in variable
+  supportersList = document.querySelector("#supporters");
+  //run function to load json
+  loadSupportersJSON();
 
-          //create bounds based on each marker
-          bounds.extend(marker.position);
-          //fit map to bounds
-          map.fitBounds(bounds);
+  google.maps.event.addDomListener(window, 'load', initialise);
 
-          //when mouse over the marker
-          google.maps.event.addListener(marker, 'mouseover', (function(marker, i) {
-            return function() {
-              //open windown with link to website
-              infowindow.setContent('<a href="'+locations[i][1]+'">'+locations[i][0]+'</a>');
-              infowindow.open(map, marker);
-            }
-          })(marker, i)); 
-        }
-      }); 
-    },
-    error: function(){
-      alert('failure');
+  //function to load json, calls processJSON function
+  function loadSupportersJSON () {
+  	jQuery.getJSON('js/maps.json', processSupportersJSON);
+  }
+
+  //process JSON
+  function processSupportersJSON(supporters) {
+    //log json in console
+  	console.log(supporters);
+    // sort supporters array from north to south
+    supporters.sort(function (supportersA, supportersB) {
+      if (supportersA.lat < supportersB.lat) {
+        return 1; // A comes first - correct order
+      } else {
+        return -1; // B comes first - reverse the order
+      }
+    });
+    //empty out supporters list
+    supportersList.innerHTML = "";
+    // setting up a var for this particular location and calling add marker function
+    for(var i = 0; i < supporters.length; i += 1) {
+      var locations = supporters[i];
+      addMarker(locations);
     }
-  });
-}
-function loadScript() {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'http://maps.googleapis.com/maps/api/js?v=3.exp' +
-      '&signed_in=true&callback=initialize';
-      https://maps.google.com/maps/api/js?sensor=true
-  document.body.appendChild(script);
-}
-document.addEventListener('DOMContentLoaded', loadScript);
+    //run function to fit markers on map
+    fitMapToBounds();
+  }
+
+  //function to fit markers to map
+  function fitMapToBounds() {
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < markerCollection.length; i += 1) {
+      bounds.extend(markerCollection[i].getPosition());
+    }
+    map.fitBounds(bounds);
+  }
+
+  function addMarker(locations) {
+    var marker = new google.maps.Marker({
+      'map': map,
+      'position': new google.maps.LatLng(locations.lat, locations.lng),
+      'title': locations.name,
+      'icon': ICONS.UNSELECTED
+    });
+
+    markerCollection.push(marker);
+
+    var listItem = document.createElement('li');
+    listItem.innerHTML = '<li><a href="'+locations.link+'">'+locations.name+'</a></li>';
+    supportersList.appendChild(listItem);
+
+    var infoWindow = new google.maps.InfoWindow({
+      'content': '<a href="'+locations.link+'">'+locations.name+'</a>'
+    });
+
+    listItem.addEventListener("mouseover", function (evt) {
+      evt.preventDefault();
+      selectMarker(marker, listItem);
+    });
+
+    google.maps.event.addDomListener(marker, "click", function(){
+      selectMarker(marker, listItem);
+      infoWindow.open(map, marker);
+    });
+  }
+
+  function selectMarker(marker, listItem) {
+    deselectAllMarkers();
+    marker.setIcon(ICONS.SELECTED);
+    marker.setZIndex(1000);
+    //map.setZoom(12);
+    map.panTo(marker.getPosition());
+    listItem.className = "active";
+  }
+
+  function deselectAllMarkers() {
+    for (var i = 0; i < markerCollection.length; i += 1) {
+      markerCollection[i].setIcon(ICONS.UNSELECTED);
+      markerCollection[i].setZIndex(null);
+    }
+    listItemCollection = supportersList.querySelectorAll('li');
+    for (var i = 0; i < listItemCollection.length; i += 1) {
+      listItemCollection[i].className = "";
+    }
+  }
+};
+
+initialise();
